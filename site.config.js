c45b47c4336e07f1287b92df4e8b20c03659b712
Yvonne JIN <55522464+Yvonne27Jin@users.noreply.github.com>
2022-02-26 00:27:15 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '8bf4911f4ffb468a97112fed7c99ab5d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
+  name: 'Yvonne Yaqi JIN',
+  domain: 'yvonne27jin',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'Personal Portfolio site of Yvonne Yaqi JIN',
+  socialImageTitle: 'Yvonne JIN',
+  socialImageSubtitle: 'Welcome! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'yvonnejin2',
+  github: 'yvonne27jin',
+  linkedin: 'yvonne-yaqi-jin-78468b180',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
